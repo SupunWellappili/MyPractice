8564c0536aacb626c0b4f807d55e50769d0cf6de
SupunWellappili <supunchanada1814@gmail.com>
2023-07-10 21:15:26 +0530

    introduction in Encapsulation

--- Example01.java
+++ Example01.java
@@ -1,7 +1,51 @@
-public class Example01 {
+class Student {
+    private String name;
+    private int age;
+    private double marks;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public double getMarks() {
+        return marks;
+    }
+
+    public void setMarks(double marks) {
+        this.marks = marks;
+    }
+}
+
+class Example01 {
     public static void main(String[] args) {
+        Student std1 = new Student();
+        std1.setName("Supun chandana");
+        std1.setAge(22);
+        std1.setMarks(76.56);
+        System.out.println(std1.getName());
+        System.out.println(std1.getAge());
+        System.out.println(std1.getMarks());
+        System.out.println();
 
 
-
+        Student std2 = new Student();
+        std2.setName("Hasanthi Kumuditha");
+        std2.setAge(24);
+        std2.setMarks(89.78);
+        System.out.println(std2.getName());
+        System.out.println(std2.getAge());
+        System.out.println(std2.getMarks());
     }
 }
--- out/production/MyPractice/README.md
+++ out/production/MyPractice/README.md
@@ -1,2 +1,5 @@
 # MyPractice
 Testing Your Coding....
+
+
+#This my first git & github project
