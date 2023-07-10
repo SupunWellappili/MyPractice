6f3d5fc51d3bfb90728331e21c652dea73a52db6
SupunWellappili <supunchanada1814@gmail.com>
2023-07-10 21:23:17 +0530

    Funny Calculator Testing

---
+++ Example02.java
@@ -0,0 +1,12 @@
+public class Example02 {
+    public static void main(String[] args) {
+        int x =100, y = 200,z;
+        x =100;
+        z=200;
+        z =x+y;
+        System.out.println(x);
+        System.out.println(y);
+        System.out.println(z);
+
+    }
+}
