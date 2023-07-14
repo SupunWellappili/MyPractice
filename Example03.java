7b0e8e1ed06da59154ed38fa5ca3855e0e9d6f44
SupunWellappili <supunchanada1814@gmail.com>
2023-07-14 22:21:46 +0530

    Total Calculator in two numbers

--- Example03.java
+++ Example03.java
@@ -10,9 +10,13 @@ public class Example03 {
 
         int num1,num2;
 
-        System.out.print("Inter Your Number : ");
+        System.out.print("Inter Your Number 01 : ");
         num1 = Integer.parseInt(bufferedReader.readLine());
 
+        System.out.print("Inter Your Number 02 : ");
+        num2 = Integer.parseInt(bufferedReader.readLine());
 
+        int tot = num1+num2;
+        System.out.println("Total is : "+tot);
     }
 }
