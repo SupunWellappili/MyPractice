1637e013a143909901ef5fec647858ced0c39a07
SupunWellappili <supunchanada1814@gmail.com>
2023-07-19 18:10:25 +0530

    create java-Flow sout Step

---
+++ Example07.java
@@ -0,0 +1,10 @@
+public class Example07 {
+    public static void main(String[] args) {
+        int i=0;
+        int count =0;
+        for (System.out.println(++count+") A "); i<10; System.out.println(++count+") C ") ){
+            System.out.println(++count+") B ");
+            i++;
+        }
+    }
+}
