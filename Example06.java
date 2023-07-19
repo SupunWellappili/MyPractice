6c279867bd0a3943b47a54ac3b56366d73c15748
SupunWellappili <supunchanada1814@gmail.com>
2023-07-19 17:57:42 +0530

    Use Java Flow Controller

---
+++ Example06.java
@@ -0,0 +1,11 @@
+public class Example06 {
+    public static void main(String[] args) {
+        int i=0;
+        for (int j = 0; j < 10; j++) {
+            int k =0;
+            System.out.println(i+" "+j+" "+k);
+            i++;k++;
+
+        }
+    }
+}
