89f49066f968eb6196b1c86ffc2ed4ab5569ce63
SupunWellappili <supunchanada1814@gmail.com>
2023-07-19 15:57:56 +0530

    sout prints

---
+++ Example05.java
@@ -0,0 +1,36 @@
+public class Example05 {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        System.out.println();
+
+        Byte x, y;
+        x = Byte.MIN_VALUE;
+        System.out.println(x);//-128
+        y = Byte.MAX_VALUE;
+        System.out.println(y);//127
+
+        System.out.println();
+
+
+        float xx, yy;
+        xx = Float.MAX_VALUE;
+        System.out.println(x);
+
+        xx = Float.MIN_VALUE;
+        System.out.println(xx);
+
+        System.out.println();
+
+        char \u0061 = '\u0061';
+        System.out.println(\u0061);
+        System.out.println();
+
+        char ch = 'A';
+        System.out.println(ch);//A
+        System.out.println(ch+1);//66
+        System.out.println("ch+1");//ch+1
+        System.out.println(ch+"1");//A1
+    }
+}
+
+
